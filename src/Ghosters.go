547b7b49c9fdf318c6e03d377eb248efbd794df6
Nick Woods <woodsnicholas01@gmail.com>
2024-07-14 22:48:40 +0100

    adapting ghost to avoid detection

--- src/Ghosters.go
+++ src/Ghosters.go
@@ -2,7 +2,14 @@ package main
 
 func Ceaser(bytes []byte) []byte {
 	for index, currbyte := range bytes {
-		bytes[index] = byte((int64(currbyte) + 6) % 256)
+		bytes[index] = byte((int(currbyte) + index) % 256)
+	}
+	return bytes
+}
+
+func ReverseCeaser(bytes []byte) []byte {
+	for index, currbyte := range bytes {
+		bytes[index] = byte((int(currbyte) + (256 - index)) % 256)
 	}
 	return bytes
 }
--- src/main.go
+++ src/main.go
@@ -22,18 +22,46 @@ func main() {
 	fmt.Println("(|  | '. (_/ |  .-.  |   \\ |  | |  |.-._)   \\    |  |    ")
 	fmt.Println(" |  '--'  |  |  | |  |    `'  '-'  '\\       /    |  |    ")
 	fmt.Println("  `------'   `--' `--'      `-----'  `-----'     `--'    ")
-	_ = os.Args
+	args := os.Args
+	reverse := false
+	inputfile := ""
+	outputfile := ""
+	for index, val := range args {
+		switch val {
+		case "-r":
+			reverse = true
+		case "-in":
+			if len(args)-1 == index {
+				fmt.Println("No input file specified with -o")
+				return
+			}
+			inputfile = args[index+1]
+			break
+		case "-out":
+			if len(args)-1 == index {
+				fmt.Println("No output file specified with -o")
+				return
+			}
+			outputfile = args[index+1]
+			break
+		}
+	}
 
-	bytes, err := ioutil.ReadFile("test.txt") //read the content of file
+	bytes, err := ioutil.ReadFile(inputfile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	fmt.Println(bytes)
-	bytes = Ceaser(bytes)
-	fmt.Println(bytes)
-	err = ioutil.WriteFile("invisible.ghost", bytes, 0777)
+	if !reverse {
+		bytes = Ceaser(bytes)
+	} else {
+		bytes = ReverseCeaser(bytes)
+	}
+	if outputfile == "" {
+		err = ioutil.WriteFile("invisible.ghost", bytes, 0777)
+	} else {
+		err = ioutil.WriteFile(outputfile, bytes, 0777)
+	}
 	if err != nil {
 		fmt.Println(err)
 	}
